3c6c401b981609cd9d8b72c65cc645a73b47a932
Olaf Lischke <olaf.lischke@lischke-edv.de>
2022-03-21 17:02:27 +0100

    Upload Seminarende

---
+++ Live Coding/MondialKonsole/MondialKonsole/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MondialKonsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load(@"C:\tmp\Buderus\CS Grundlagen\Live Coding\MondialKonsole\MondialKonsole\Data\mondial.xml");
+
+            var qPopulation = document.Root.Descendants().Where(co => co.Name.LocalName == "country")
+                                                        .Select(co => new
+                                                        // Projektion
+                                                        {
+                                                            Name = co.Elements("name").First().Value,
+                                                            Population = Convert.ToInt32(co.Elements("population").Last().Value)
+                                                        })
+                                                        .OrderByDescending(land => land.Population)
+                                                        .Take(10);
+
+            foreach (var item in qPopulation)
+            {
+                Console.WriteLine($"{item.Name}: {item.Population:#,##0}");
+            }
+
+            Console.WriteLine();
+
+            // Nullables in C#
+            int a = 12;
+            System.Nullable<int> b = null; // Nullable int
+            int? c = null;
+
+            a = b.HasValue ? b.Value : -99; // =WENN(Prüfung;Dann;Sonst)
+            a = b ?? -99;
+
+
+
+            var qCapitalsWithB = document.Root.Descendants("country")
+                                    .Select(co => new
+                                    {
+                                        Name = co.Elements("name").First().Value,
+                                        Capital = co.Descendants("city")
+                                                    .Where(cty => cty.Attribute("id").Value == co.Attribute("capital")?.Value)
+                                                    .FirstOrDefault()?
+                                                    .Elements("name").First().Value ?? ""
+                                    })
+                                    .Where(cp => cp.Capital.StartsWith("B"));
+
+            foreach (var item in qCapitalsWithB)
+            {
+                Console.WriteLine($"{item.Name}: {item.Capital}");
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+    //public  class CountryDesc
+    //{
+    //    public int Population { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ Live Coding/Zahlenraten/Zahlenraten/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zahlenraten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Computer "denkt" sich eine Zahl
+            Random random = new Random();
+            int computerZahl = random.Next(1, 11);
+
+            // Benutzer hat 3 Versuche, diese zu raten
+            int benutzerZahl;
+            int i = 0;
+            do
+            {
+                Console.WriteLine($"Versuch Nr. {i + 1}: Bitte rate eine Zahl:");
+                benutzerZahl = Convert.ToInt32(Console.ReadLine());
+                i++;
+
+            } while (computerZahl != benutzerZahl && i<3); // "fußgesteuert"
+
+            //while (true) // "kopfgesteuert"
+            //{
+
+            //}
+
+
+            // Immer: "Die richtige Zahl war..."
+            Console.WriteLine($"Die richtige Zahl war {computerZahl}.");
+
+            Console.ReadKey();
+        }
+
+        static void ZahlenRatenFor(string[] args)
+        {
+            // Computer "denkt" sich eine Zahl
+            Random random = new Random();
+            int computerZahl = random.Next(1, 11);
+
+            // Benutzer hat 3 Versuche, diese zu raten
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Versuch Nr. {i + 1}: Bitte rate eine Zahl:");
+                int benutzerZahl = Convert.ToInt32(Console.ReadLine());
+                // wenn richtig geraten: Herzlichen Glückwunsch!
+                if (benutzerZahl == computerZahl)
+                {
+                    Console.WriteLine("Herzlichen Glückwunsch!");
+                    break;
+                }
+            }
+
+            // Immer: "Die richtige Zahl war..."
+            Console.WriteLine($"Die richtige Zahl war {computerZahl}.");
+
+            Console.ReadKey();
+        }
+
+        static void EvaPrinzip(string[] args)
+        {
+            // Eingabe
+            Console.Write("Wie geht's Dir heute?");
+
+            string antwort = Console.ReadLine();
+
+            // Verarbeitung
+            if (antwort.ToLower().Contains("gut"))
+            {
+                // Ausgabe
+                Console.WriteLine("Prima!");
+            }
+
+            // Damit die Konsole sich nicht sofort schließt bei Programmende
+            Console.ReadKey();
+        }
+    }
+}
